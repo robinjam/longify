ec4d025c867a5537db09a303012cdb1a53529739
James Robinson <admin@robinjam.net>
2012-02-07 20:57:44 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Longify::Application.config.secret_token = 'efa4d32cb7e59ebf430137532a4d043cd74914824555604914097a005ea5032fe997db4a3c310a3f63f5ece320d9d1f83d39aa81e07d9d51eb8e0151ba51775b'
