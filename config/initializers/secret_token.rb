14b3aebde5b5ef90e797b47384f7f2f1f013d69b
James Robinson <admin@robinjam.net>
2012-02-09 08:10:39 +0000

    Allow customisation of secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Longify::Application.config.secret_token = 'efa4d32cb7e59ebf430137532a4d043cd74914824555604914097a005ea5032fe997db4a3c310a3f63f5ece320d9d1f83d39aa81e07d9d51eb8e0151ba51775b'
+Longify::Application.config.secret_token = ENV['SECRET_TOKEN'] || 'efa4d32cb7e59ebf430137532a4d043cd74914824555604914097a005ea5032fe997db4a3c310a3f63f5ece320d9d1f83d39aa81e07d9d51eb8e0151ba51775b'
